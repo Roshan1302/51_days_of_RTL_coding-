726763673a3ddcdec134612c89a28abae9abd781
Roshan Ekre <118035816+Roshan1302@users.noreply.github.com>
2024-08-18 18:12:08 +0530

    Add files via upload

---
+++ day_32(carry select adder )/carry_select_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.03.2024 11:05:27
+// Design Name: 
+// Module Name: carry_select_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module carry_select_tb(
+
+    );
+endmodule
