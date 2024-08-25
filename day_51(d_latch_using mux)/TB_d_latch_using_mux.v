a13cf3cb19f39920abdd6afaecbaca99cdc1420b
Roshan Ekre <118035816+Roshan1302@users.noreply.github.com>
2024-08-25 08:07:36 +0530

    Add files via upload

---
+++ day_51(d_latch_using mux)/TB_d_latch_using_mux.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.08.2024 15:19:37
+// Design Name: 
+// Module Name: TB_d_latch_using_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TB_d_latch_using_mux(
+
+    );
+endmodule
